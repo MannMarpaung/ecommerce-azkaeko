9601b95efaad5cc56aec0a38fb942183c3b0189d
Mann Marpaung <mannmarpaung@gmail.com>
2025-08-19 09:27:12 +0700

    sistem role

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'admin' => App\Http\Middleware\IsAdmin::class,
+            'seller' => App\Http\Middleware\IsSeller::class,
+            'user' => App\Http\Middleware\IsUser::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/cart', [App\Http\Controllers\CartController::class, 'index'])->name('cart.index');
     Route::post('/cart/{product}', [App\Http\Controllers\CartController::class, 'store'])->name('cart.store');
 });
+
+Route::group(['middleware' => ['auth', 'admin']], function () {});
+Route::group(['middleware' => ['auth', 'seller']], function () {});
+Route::group(['middleware' => ['auth', 'user']], function () {});
